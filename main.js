d4d1cf851750ecd2f240b46746d48a0e79e59001
andrino243 <161470691+andrino243@users.noreply.github.com>
2025-04-01 16:07:27 +0100

    Evenement sidebar

--- main.js
+++ main.js
@@ -16,3 +16,26 @@ const closeNav = () => {
 
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav);
+
+
+
+
+const sidebar = document.querySelector('aside');
+const showsidebarBtn = document.querySelector('#show__sidebar-btn');
+const hidesidebarBtn = document.querySelector('#hide__sidebar-btn');
+
+// shows sidebar on small devices//
+const showSidebar = () => {
+    sidebar.style.left = '0';
+    showsidebarBtn.style.display = 'none';
+    hidesidebarBtn.style.display = 'inline-block';
+}
+
+// shows sidebar on small devices//
+const hideSidebar = () => {
+    sidebar.style.left = '-100%';
+    showsidebarBtn.style.display = 'inline-block';
+    hidesidebarBtn.style.display = 'none';
+}
+showsidebarBtn.addEventListener('click',showSidebar);
+hidesidebarBtn.addEventListener('click',hideSidebar);
